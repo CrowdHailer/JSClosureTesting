cc68800bfbd183d20976b8d2299d1bf90cb4c75e
CrowdHailer <petersaxton@londonlayout.co.uk>
2014-04-15 18:23:50 +0100

    Added fizzbuzz closure to demonstrate tests

---
+++ src/fizzbuzz.js
@@ -0,0 +1,34 @@
+function fizzbuzzGenerator () {
+  function isDivisibleByThree (number) {
+    return number % 3 === 0;
+  }
+  function isDivisibleByFive (number) {
+    return number % 5 === 0;
+  }
+  function isDivisibleByFifteen (number) {
+    return number % 15 === 0;
+  }
+
+  function fizzbuzz (n) {
+    if (isDivisibleByFifteen(n)) {
+      return "fizzbuzz";
+    } else if (isDivisibleByThree(n)) {
+      return "fizz";
+    } else if (isDivisibleByFive(n)) {
+      return "buzz";
+    } else {
+      return n ;
+    };
+  }
+  /*For Tests*/
+  if (env.JSClosureTest) {
+    fizzbuzz._test = {
+      isDivisibleByThree: isDivisibleByThree,
+      isDivisibleByFive: isDivisibleByFive,
+      isDivisibleByFifteen: isDivisibleByFifteen
+    }
+  };
+  /*End Tests*/
+
+  return fizzbuzz;
+ }
